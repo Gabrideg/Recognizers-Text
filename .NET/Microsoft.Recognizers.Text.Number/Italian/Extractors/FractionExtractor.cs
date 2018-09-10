e058011c39ae48f907869c274d622d2bd7bd9ba4
Gabriele de Gregorio Mangione <gabdgm@microsoft.com>
2018-09-10 16:53:45 +0200

    add YAML for italian numbers

---
+++ .NET/Microsoft.Recognizers.Text.Number/Italian/Extractors/FractionExtractor.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+using Microsoft.Recognizers.Definitions.Italian;
+
+namespace Microsoft.Recognizers.Text.Number.Italian
+{
+    public class FractionExtractor : BaseNumberExtractor
+    {
+        internal sealed override ImmutableDictionary<Regex, string> Regexes { get; }
+
+        protected sealed override NumberOptions Options { get; }
+
+        protected sealed override string ExtractType { get; } = Constants.SYS_NUM_FRACTION; // "Fraction";
+
+        private static readonly ConcurrentDictionary<(NumberOptions, string), FractionExtractor> Instances =
+            new ConcurrentDictionary<(NumberOptions, string), FractionExtractor>();
+
+        public static FractionExtractor GetInstance(NumberOptions options = NumberOptions.None, string placeholder = "")
+        {
+            var cacheKey = (options, placeholder);
+            if (!Instances.ContainsKey(cacheKey))
+            {
+                var instance = new FractionExtractor(options);
+                Instances.TryAdd(cacheKey, instance);
+            }
+
+            return Instances[cacheKey];
+        }
+
+        private FractionExtractor(NumberOptions options)
+        {
+            Options = options;
+
+            var regexes = new Dictionary<Regex, string>
+            {
+                {
+                    new Regex(NumbersDefinitions.FractionNotationWithSpacesRegex,
+                        RegexOptions.IgnoreCase | RegexOptions.Singleline),
+                    Constants.FRACTION_IN_PURENUMBER
+                },
+                {
+                    new Regex(NumbersDefinitions.FractionNotationRegex,
+                        RegexOptions.IgnoreCase | RegexOptions.Singleline),
+                    Constants.FRACTION_IN_PURENUMBER
+                },
+                {
+                    new Regex(
+                        NumbersDefinitions.FractionNounRegex,
+                        RegexOptions.IgnoreCase | RegexOptions.Singleline),
+                    Constants.FRACTION_WITH_CONNECTOR
+                },
+                {
+                    new Regex(
+                        NumbersDefinitions.FractionNounWithArticleRegex,
+                        RegexOptions.IgnoreCase | RegexOptions.Singleline),
+                    Constants.FRACTION_WITH_CONNECTOR
+                }
+            };
+
+            if ((Options & NumberOptions.PercentageMode) != 0)
+            {
+                regexes.Add(
+                    new Regex(
+                        NumbersDefinitions.FractionPrepositionWithinPercentModeRegex,
+                        RegexOptions.IgnoreCase | RegexOptions.Singleline),
+                    "FracEng"
+                );
+            }
+            else
+            {
+                regexes.Add(
+                    new Regex(
+                        NumbersDefinitions.FractionPrepositionRegex,
+                        RegexOptions.IgnoreCase | RegexOptions.Singleline),
+                    "FracEng"
+                );
+            }
+
+            Regexes = regexes.ToImmutableDictionary();
+        }
+    }
+}
---
+++ .NET/Microsoft.Recognizers.Text.Number/Italian/Parsers/ItalianNumberRangeParserConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Recognizers.Definitions.Italian;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Recognizers.Text.Number.Italian
+{
+    public class ItalianNumberRangeParserConfiguration : INumberRangeParserConfiguration
+    {
+        public CultureInfo CultureInfo { get; private set; }
+
+        public IExtractor NumberExtractor { get; private set; }
+
+        public IExtractor OrdinalExtractor { get; private set; }
+
+        public IParser NumberParser { get; private set; }
+
+        public Regex MoreOrEqual { get; private set; }
+
+        public Regex LessOrEqual { get; private set; }
+
+        public Regex MoreOrEqualSuffix { get; private set; }
+
+        public Regex LessOrEqualSuffix { get; private set; }
+
+        public Regex MoreOrEqualSeparate { get; private set; }
+
+        public Regex LessOrEqualSeparate { get; private set; }
+
+        public ItalianNumberRangeParserConfiguration() : this(new CultureInfo(Culture.Italian))
+        {
+        }
+
+        public ItalianNumberRangeParserConfiguration(CultureInfo ci)
+        {
+            CultureInfo = ci;
+
+            NumberExtractor = Italian.NumberExtractor.GetInstance();
+            OrdinalExtractor = Italian.OrdinalExtractor.GetInstance();
+            NumberParser = new BaseNumberParser(new ItalianNumberParserConfiguration());
+            //MoreOrEqual = new Regex(NumbersDefinitions.MoreOrEqual, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            //LessOrEqual = new Regex(NumbersDefinitions.LessOrEqual, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            //MoreOrEqualSuffix = new Regex(NumbersDefinitions.MoreOrEqualSuffix, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            //LessOrEqualSuffix = new Regex(NumbersDefinitions.LessOrEqualSuffix, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            //MoreOrEqualSeparate = new Regex(NumbersDefinitions.OneNumberRangeMoreSeparateRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            //LessOrEqualSeparate = new Regex(NumbersDefinitions.OneNumberRangeLessSeparateRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        }
+    }
+}
